ba09d8cbab4fabd039a1aaa89e7ab0d5c544658f
Sofia Craciun <sofia.craciun@ysura.com>
2016-07-09 22:33:55 +0200

    Creating some entities at startup.

--- src/main/java/eu/marmotlabs/planner/startup/StartupListener.java
+++ src/main/java/eu/marmotlabs/planner/startup/StartupListener.java
@@ -1,14 +1,27 @@
 package eu.marmotlabs.planner.startup;
 
+import eu.marmotlabs.planner.domain.Product;
+import eu.marmotlabs.planner.domain.enumeration.UnitOfMeasure;
+import eu.marmotlabs.planner.service.ProductService;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
+
 @Component
 public class StartupListener implements ApplicationListener<ContextRefreshedEvent> {
 
+    @Inject
+    ProductService productService;
+
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
+        Product milk = new Product();
 
+        milk.setName("Milk");
+        milk.setUnitOfMeasure(UnitOfMeasure.ML);
+
+        productService.save(milk);
     }
 }
